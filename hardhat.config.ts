4b570055cecfba6c51b80501658820b6ae4eb2f5
junse0m <quasar125@naver.com>
2024-11-16 03:38:33 +0900

    pk security

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,14 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+const pk = process.env.PK!;
+
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
   networks: {
     goerli: {
         url: "https://eth-goerli.alchemyapi.io/v2/YOUR_ALCHEMY_API_KEY",
-        accounts: ["d02e759ca27aa5fcbd9d4f8eb779f6e20c9c515722332b47a4ee37357a5e6a62"] // 배포 계정의 개인 키
+        accounts: [pk] // 배포 계정의 개인 키
     },
   }
 };
